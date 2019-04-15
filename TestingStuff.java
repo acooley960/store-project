1dc433a79029183298cd00b09229f1f992cd03e8
Aaron C <49112275+acooley960@users.noreply.github.com>
2019-04-14 22:55:30 -0600

    Add files via upload
    A driver class for testing purposes

---
+++ TestingStuff.java
@@ -0,0 +1,13 @@
+package store;
+
+public class TestingStuff
+{
+
+	public static void main( String[ ] args )
+	{
+		// create and print toString of a keyboard
+		Keyboard keyboard1 = new Keyboard ( "Razer", "Blackwidow Ultimate", 6, 69.99, 7 );
+		System.out.println ( keyboard1.toString ( ) );
+	}
+
+}
