48a5bec0e6883be45d93a34c0c541ab36acdafe5
SoldierMedic <47678911+SoldierMedic@users.noreply.github.com>
2019-04-17 22:21:00 -0600

    Add files via upload
    added getKeyboard method and tested it

--- TestingStuff.java
+++ TestingStuff.java
@@ -12,19 +12,43 @@ import java.io.*;
 
 public class TestingStuff
 {
-
+	public static Scanner input;
 	public static void main( String[ ] args ) throws FileNotFoundException
 	{
+		input = new Scanner ( System.in );
 		// create and print toString of a keyboard
-		Keyboard keyboard1 = new Keyboard ( "Razer", "Blackwidow Ultimate", 6, 69.99, "Razer Mechanical" );
-		System.out.println ( keyboard1.toString ( ) );
-		
-		
+		Keyboard razer= new Keyboard ( "Razer", "Blackwidow Ultimate","Razer Mechanical Green", 6, 69.99 );
+		System.out.println ( razer.toString ( ) );
+		Keyboard keyboard1 = getKeyboard();
+		System.out.println (  );
+		Keyboard keyboard2 = getKeyboard();
+		System.out.println (  );
+		Keyboard keyboard3 = getKeyboard();
 		// Aaron: 16 Apr
-		Scanner readFile = new Scanner(new File("keyboards.txt"));
-		
-		
+		Scanner readFile = new Scanner(new File("src/store/keyboards.txt"));//may need to change the directory in order to work
 		readFile.close ( );
+	}
+	// Marco : 17 APR 19
+	public static Keyboard getKeyboard()
+	{
+		
+		System.out.println ( "What is the brand of keyboard?" );
+			String brand = input.next ( );
+			System.out.println ( "What is the model of keyboard?" );
+			String model = input.next ( );
+			System.out.println ( "What are the types of switches?" );
+			String switchType = input.next( );
+			System.out.println ( "How many do you have in stock?" );
+			int stock = input.nextInt( );
+			System.out.println ( "What is the price?" );
+			double price = input.nextDouble( );
+			Keyboard kb1 = new Keyboard(brand, model, switchType, stock, price);
+
+		return kb1;
+		
 	}
 
 }
+/*
+ * 17APR19MARCO: added getKeyboard method and added lines 21-26
+ */
